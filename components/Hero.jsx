b64a94a74a5d9b0fdda2f03c93ffbcd5bd32b3ad
Sergio Benzo Barrado <dvbxhjj@gmail.com>
2026-04-07 20:30:32 +0600

    Create Hero.jsx

---
+++ components/Hero.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div style={{padding:"50px", textAlign:"center"}}>
+      <h1>Welcome to My Website</h1>
+      <p>I build modern React websites</p>
+    </div>
+  );
+}
